14f6c9ee3e6098c38a9845f1668b08510abbdbf6
Jairo <montenegroxnj@gmail.com>
2015-10-30 00:19:28 -0400

    created tasks scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 
+  resources :tasks
   devise_for :users
   devise_for :installs
   # The priority is based upon order of creation: first created -> highest priority.
